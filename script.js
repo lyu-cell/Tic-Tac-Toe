ca1bc85e1a9ff79b19aa13ee47fb60479d9eaf8d
lyu-cell <shizueizawa340@gmail.com>
2024-02-02 02:44:17 +0600

    announcer function is added that gives the final result after 3 match has been played or in case of one player consicutivly winning 2 matches the winner is immediatly declared without the third match

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const Gameboard = (function () {
 
     let player1x = [];
+    let scoreBox1 = 0;
     let player2o = [];
+    let scoreBox2 = 0;
+    let drawScore = 0;
 
     const gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    function gamePatCall(playerVar) {
+    const gamePatCall = function(playerVar) {
         
         if (playerVar.includes(1) === true && playerVar.includes(2) === true && playerVar.includes(3) === true || playerVar.includes(4) === true && 
             playerVar.includes(5) === true && playerVar.includes(6) === true || playerVar.includes(7) === true && playerVar.includes(8) === true &&
@@ -14,26 +17,70 @@ const Gameboard = (function () {
             playerVar.includes(6) === true && playerVar.includes(9) === true || playerVar.includes(1) === true && playerVar.includes(5) === true && 
             playerVar.includes(9) === true || playerVar.includes(3) === true && playerVar.includes(5) === true && playerVar.includes(7)) {
             console.log(player2o, player1x)
-            if (playerVar === player1x) return console.log(playerVar), player1x = [], player2o = [], "Player1 is The Winner!";
-            else console.log(playerVar), player1x = [], player2o = [], "player2 is The WInner!";
+
+            if (playerVar === player1x) {
+                console.log(playerVar)
+                scoreBox1++
+                
+                if ((player1x.length + player2o.length) === 9 && (drawScore + scoreBox1 + scoreBox2) === 3 
+                    || scoreBox1 === 2 || scoreBox2 === 2) return announcer();
+                else player1x = [], player2o = [], console.log("Player1 is The Winner!");
+            } 
+            else {
+                console.log(playerVar)
+                scoreBox2++;
+                if ((player1x.length + player2o.length) === 9 && (drawScore + scoreBox1 + scoreBox2) === 3 ||
+                    scoreBox2 === 2 || scoreBox1 === 1) return announcer();
+                else return player1x = [], player2o = [], console.log("Player2 is The Winner!");
+            }
         }
 
-        else if (player1x.length === 5 || player2o.length === 5) {
-            console.log(playerVar), player1x = [], player2o = [] 
-             
-            return "Draw";
+        else if (player1x.length === 5 && player2o.length === 4 || player2o.length === 5 && player1x.length === 4) {
+            
+            console.log(playerVar)
+            drawScore++
+            if ((player1x.length + player2o.length) === 9 && (drawScore + scoreBox1 + scoreBox2) === 3 ) return announcer();
+            else return player1x = [], player2o = [], console.log("Draw", drawScore + scoreBox1 + scoreBox2, player1x.length + player2o.length)
         } 
     }
-
     
+    const announcer = function() {
+
+        if (scoreBox1 > scoreBox2) {
+            
+            scoreBox1 = 0;
+            scoreBox2 = 0;
+            drawScore = 0;
+            
+            return console.log("player1 Congratulation, You Won The Game!");
+        } 
+
+        else if (scoreBox2 > scoreBox1) {
+            
+            scoreBox1 = 0
+            scoreBox2 = 0
+            drawScore = 0
+            
+            return console.log("Player2 Congratulation, You Won The Game!")
+        } 
+        
+        else if (drawScore > (scoreBox1 + scoreBox2)) {
+            
+            scoreBox1 = 0
+            scoreBox2 = 0
+            drawScore = 0
+            
+            return console.log("Game Draw!");
+        } 
+    }
     
     const invokeJudge  = function(square, variable) {
-        
+
         if (variable.length < 3 || player1x.length === 3 || player2o.length === 3 || player1x.length > 3 || player2o.length > 3) {
             variable.push(square);
-            console.log(player1x, player2o)
             if (player1x.length === 3 || player2o.length === 3 || player1x.length === 5 || player2o.length === 5) {
-                return gamePatCall(variable)
+
+                gamePatCall(variable);
             }
         }
     }
@@ -66,61 +113,64 @@ const Gameboard = (function () {
 })()
 
 
-// the winning pattern data set will have 10 sets, in each of which there will be three numbers.
-// if the numbers in the player array matches with any of the data win set, then that player will be the winner.
-// a program should be made which will check every index numbers of the players array to find the winning
-// numbers.
+
+/*
+
+Gameboard.player1s(5)
+
+Gameboard.player1s(3)
+
+Gameboard.player1s(4)
+
+Gameboard.player1s(8)
+
+Gameboard.player2s(1)
+
+Gameboard.player2s(2)
+
+Gameboard.player2s(7)
+
+Gameboard.player2s(6)
+
+Gameboard.player2s(9)
 
 
 
+Gameboard.player1s(2)
+
+Gameboard.player1s(3)
+
+Gameboard.player1s(8)
+
+Gameboard.player1s(9)
+
+Gameboard.player2s(1)
+
+Gameboard.player2s(5)
+
+Gameboard.player2s(6)
+
+Gameboard.player2s(7)
+
+Gameboard.player2s(4)
 
 
 
+Gameboard.player2s(1)
 
+Gameboard.player2s(2)
 
+Gameboard.player2s(7)
 
+Gameboard.player2s(6)
 
+Gameboard.player2s(9)
 
+Gameboard.player1s(5)
 
+Gameboard.player1s(3)
 
+Gameboard.player1s(4)
 
-
-
-
-
-
-
-
-/* 
-1. tic tac toe project doesn't require an ai or anything.
-
-2. two players should be able to play tic tac toe
-_____________________________________________
-
-if i give gameboard array of tictactoe a series of numbers from 1-9 each
-of which represent a square on the board.
-
-players are gonna chose any one of these numbers and that chosen number will
-represent that persons chosen square.
-
-__________________________________________________________
-
-Algorithm:
-
-1. gameboard array that have numbers from 1-9.
-
-2. player function clicks a square which represent a number in the gameboard array.
-
-3. the player function sends its input to the gameJudge.
-
-4. if it matches with the one of the unfilled square of gameboard then it accepts it.
-
-5. if not then it doesn't accept the input.
-
-6. 
-
-2. gameJudge object that has function that takes the inputed number from 
-player and matches it with the gameboard array. 
-
-
-*/
+Gameboard.player1s(8)
+ */
